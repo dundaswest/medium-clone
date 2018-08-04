4f04ca6a8afe6725fdea4de2580082d6fd972375
Eunbyul Cho <hello@eunbyulcho.me>
2018-08-04 22:31:52 +0900

    add webpeck, App component

---
+++ client/src/js/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    state: {
+    }
+  }
+
+  render() {
+    return (
+      <div> hi </div>
+    );
+  }
+}
+
+export default App;
