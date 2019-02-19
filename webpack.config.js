6934bd7e6f1e2dd4556629a69935f246c95bad32
Eunbyul Cho <hello@eunbyulcho.me>
2019-02-19 19:17:26 +0900

    add output.publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
   resolve: { extensions: ['.js', '.jsx'] },
   output: {
     path: path.resolve(__dirname, 'public'),
+    publicPath: '/',
     filename: 'bundle.js',
   },
   devServer: {
