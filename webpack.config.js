c9afe26eb55be7846fa6a1de581452faebdc2d84
Eunbyul Cho <hello@eunbyulcho.me>
2019-05-28 23:57:06 +0900

    when production, stringfy

--- webpack.config.js
+++ webpack.config.js
@@ -77,7 +77,7 @@ module.exports = {
         NODE_ENV: JSON.stringify('production'),
       },
     }),
-    new webpack.optimize.DedupePlugin(), // dedupe similar code
+
     new webpack.optimize.UglifyJsPlugin(), // minify everything
     new webpack.optimize.AggressiveMergingPlugin(), // Merge chunks
   ],
