dbb0c4012198d6883dc76f420564ef10c146d0a5
Eunbyul Cho <hello@eunbyulcho.me>
2019-05-29 00:51:00 +0900

    change to preact

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,20 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const srcFolder = path.join(__dirname, '/client/src/');
 const webpack = require('webpack');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-3-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
   entry: {
     main: [path.join(srcFolder, '/js/index.jsx')],
   },
-  resolve: { extensions: ['.js', '.jsx'] },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      react: 'preact-compat',
+      'react-dom': 'preact-compat',
+    },
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
     publicPath: '/',
@@ -21,6 +28,7 @@ module.exports = {
 
     historyApiFallback: true,
   },
+
   optimization: {
     minimizer: [new UglifyJsPlugin()],
   },
@@ -68,6 +76,12 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.join(srcFolder, '/index.html'),
       filename: './index.html',
+      minify: {
+        collapseWhitespace: true,
+        collapseInlineTagWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+      },
     }),
     new MiniCssExtractPlugin({
       filename: '[name].css',
@@ -77,7 +91,23 @@ module.exports = {
         NODE_ENV: JSON.stringify('production'),
       },
     }),
-
+    new CompressionPlugin({
+      filename: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.js$|\.css$|\.html$|\.eot?.+$|\.ttf?.+$|\.woff?.+$|\.svg?.+$/,
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
     new webpack.optimize.AggressiveMergingPlugin(), // Merge chunks
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        warnings: false,
+        ie8: false,
+        output: {
+          comments: false,
+        },
+      },
+    }),
+    new webpack.HashedModuleIdsPlugin(),
   ],
 };
